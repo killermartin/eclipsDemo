3259a2325ab7b3239754019d07f9b8b30ab2299f
killermartin <martingayen@gmail.com>
2019-12-30 00:30:09 +0530

    first commit

---
+++ bootjpa/src/main/java/com/example/bootjpa/controller/AlienController.java
@@ -0,0 +1,33 @@
+package com.example.bootjpa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.bootjpa.dao.AlienRepo;
+import com.example.bootjpa.model.Alien;
+
+
+
+@Controller
+public class AlienController {
+	
+	
+
+	@Autowired
+	AlienRepo repo;
+
+	@RequestMapping("/")
+	public String home()
+	{
+		return "home.jsp";
+	}
+	
+	@RequestMapping("/addAlien")
+	public String addAlien(Alien alien)
+	{
+		repo.save(alien);
+		return "home.jsp";
+	}
+
+}
